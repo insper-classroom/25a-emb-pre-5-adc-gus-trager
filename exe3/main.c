f8db2d74d8a2d9befe7ab8f1b2698f01b1b6f65c
Your Name <you@example.com>
2025-04-07 15:34:47 -0300

    Ex3.6

--- exe3/main.c
+++ exe3/main.c
@@ -2,57 +2,45 @@
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 #include "data.h"
+
 QueueHandle_t xQueueData;
 
 void data_task(void *p) {
     vTaskDelay(pdMS_TO_TICKS(400));
-
     int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
     for (int i = 0; i < data_len; i++) {
         xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
     }
-
     while (true) {
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void process_task(void *p) {
-    int total_samples = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
-    int total_averages = total_samples - 5 + 1;
-    int averages_printed = 0;
-
+    int total_samples = 11;
+    int samples_received = 0;
     int buffer[5] = {0};
-    int idx = 0;  
-    int sum = 0;   
-    int count = 0; 
-
-    int data = 0;
-    while (averages_printed < total_averages) {
+    int idx = 0, sum = 0, data = 0;
+    while (samples_received < total_samples) {
         if (xQueueReceive(xQueueData, &data, pdMS_TO_TICKS(100))) {
-            if (count < 5) {
+            samples_received++;
+            if (samples_received <= 5) {
                 buffer[idx] = data;
                 sum += data;
                 idx = (idx + 1) % 5;
-                count++;
-                if (count == 5) {
-                    int media = sum / 5;
-                    printf("%d\n", media);
-                    averages_printed++;
+                if (samples_received == 5) {
+                    printf("%d\n", sum / 5);
                 }
             } else {
                 sum -= buffer[idx];
                 buffer[idx] = data;
                 sum += data;
                 idx = (idx + 1) % 5;
-                int media = sum / 5;
-                printf("%d\n", media);
-                averages_printed++;
+                printf("%d\n", sum / 5);
             }
             vTaskDelay(pdMS_TO_TICKS(50));
         }
@@ -62,14 +50,9 @@ void process_task(void *p) {
 
 int main() {
     stdio_init_all();
-
     xQueueData = xQueueCreate(64, sizeof(int));
-
     xTaskCreate(data_task, "Data task", 4096, NULL, 1, NULL);
     xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
-
     vTaskStartScheduler();
-
-    while (true)
-        ;
+    while (true);
 }
